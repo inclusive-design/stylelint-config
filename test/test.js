541b470dc6c0677e353970f501a686b0f907b4b5
Ned Zimmerman <nzimmerman@ocadu.ca>
2026-04-28 10:01:08 -0300

    fix: resolve linting issues

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
-import config from '../index.js';
 import test from 'node:test';
 import assert from 'node:assert';
+import config from '../index.js';
 
 test('stylelintConfig', async () => {
 	assert.ok(typeof config === 'object');
